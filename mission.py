ef9422f7538ed717832054574a9ae3bb2ebb08a7
dohwan <dh990921@naver.com>
2023-01-13 15:55:16 +0900

    mission2

--- mission.py
+++ mission.py
@@ -1,10 +1,13 @@
-import random
+small = True
+green = True
 
-secret = random.randint(1, 10)
-guess = int(input("1~10 사이의 숫자를 입력하시오: "))
-if secret > guess:
-    print(f"{secret} 보다 too low") # secret 확인 위해 fstring 사용
-elif secret < guess:
-    print(f"{secret} 보다 too high")
+if small:
+    if green:
+        print("pea")
+    else:
+        print("cherry")
 else:
-    print(f"{secret} 과 just right")
+    if green:
+        print("watermelon")
+    else:
+        print("pumpkin")

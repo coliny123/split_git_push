99f9b663eb534b64e057561b1fd1adf61ba739e6
dohwan <dh990921@naver.com>
2023-01-16 13:27:20 +0900

    day03 split and join

--- day03.py
+++ day03.py
@@ -1,3 +1,12 @@
+song = """When an eel grabs your arm,
+And it causes great harm
+That's - a moray!"""
+song_list = song.split() #list와 split으로 단어 구분
+print(song_list)
+song_list[14] = song_list[14].title() #moray 위치 파악해서 리스트바꿈
+song_string = " ".join(song_list) # string으로 다시 묶기
+print(song_list)
+
 print("""
 hello
 hi
@@ -22,3 +31,32 @@ start = "나 " * 4 + "\n"
 middle = "헤이 " *3 + "\n"
 end = "안녕"
 print(start + middle + end)
+
+#대화식 인터프리터가 교체 결과만출력하기 때문에 name에 저장된 값은 바뀌지 않는다.
+name = "Henny"
+name.replace("H", "P")
+"P" + name[1:]
+
+univ = "Inha University"
+print(univ[5:])
+print(univ[5:14]) #end-1까지 가져와서 짤림
+print(univ[::2]) #0번부터 2번째씩
+# 역방향 참조
+print(univ[-10:])
+print(univ[5:-6])
+print(univ[-10:-6])
+
+pokemons_list = ["피카츄", "꼬부기", "파이리", "이상해씨"]
+pokemons_string = ", ".join(pokemons_list)
+print(pokemons_list)
+print(pokemons_string) #list에서 string으로 변경됨 "string".join(list)해서 정말 많이 쓰는 함수!
+
+inha = "a duck goes into a sea"
+print(inha.replace("a", "a nice"))
+print(inha.replace("a ", "a nice"))
+
+subject = "python, data structure, database"
+print(subject.find("data"), subject.index("data"))
+print(subject.find("inha")) #-1 던짐
+print(subject.index("inha")) #오류던짐
+print(subject.rfind("data"), subject.rindex("data")) #  문자열 끝에서 부분 문자열을 찾아서 오프셋을 얻음

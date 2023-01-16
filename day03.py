1d9e8b4a9e5f1f7af84a8caa206255a3100f9741
dohwan <dh990921@naver.com>
2023-01-16 10:14:11 +0900

    day03 print string

---
+++ day03.py
@@ -0,0 +1,24 @@
+print("""
+hello
+hi
+my name is dohwan
+ye
+""")
+print("i'm a boy")
+print('i\'m a boy')
+print('he said "hello"')
+army ='안녕하세요' \
+      '반가워요' \
+      '잘있어요' \
+      '다시만나요'
+army2 ="안녕하세요\n반가워요\n잘있어요\n다시만나요"
+army3 ="안녕하세요\t반가워요\t잘있어요\t다시만나요"
+
+print(army)
+print(army2)
+print(army3)
+
+start = "나 " * 4 + "\n"
+middle = "헤이 " *3 + "\n"
+end = "안녕"
+print(start + middle + end)
